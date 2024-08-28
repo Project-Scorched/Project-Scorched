99351cad8a7d5638d0e213e525155ae6ff605ff6
Scout <59450096+Scouter407@users.noreply.github.com>
2024-08-28 19:39:58 -0400

    Initial

---
+++ @PSC/addons/PSC_Core/MuzzleFlashes/config.cpp
@@ -0,0 +1,14 @@
+class CfgPatches {
+    class PSCMuzzleFlash {
+		units[] 			= {};
+		weapons[] 			= {};																								
+		requiredVersion 	= 0.1;
+		version				= 0.1;
+		requiredAddons[] 	= {"PSC_Core"};
+		addonRootClass      = "PSC_Core";
+		author 				= "Scout";
+		fileName 			= "PSC_Core.pbo";
+    };
+};
+
+#include "Particles.hpp"
