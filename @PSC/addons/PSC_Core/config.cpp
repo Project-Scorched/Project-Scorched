5720568a541abbb967faddb5703415c3552d0044
Scout <59450096+Scouter407@users.noreply.github.com>
2024-08-28 20:03:43 -0400

    core config + placeholder icon

---
+++ @PSC/addons/PSC_Core/config.cpp
@@ -0,0 +1,26 @@
+class CfgPatches {
+    class PSC_Core {
+		units[] 			= {};
+		weapons[] 			= {};																								
+		requiredVersion 	= 0.1; // Don't Touch
+		version				= 0.1;
+		requiredAddons[] 	= { // Loads all of Arma + CBA
+                                "cba_main",
+                                "A3_Data_F_AoW_Loadorder"
+                              };
+		addonRootClass      = "PSC_Core"; // Subconfigs 
+		author 				= "Project Scorched Team"; // Team name
+		fileName 			= "PSC_Core.pbo"; // File name
+    };
+};
+
+class CfgMods
+{
+	class Mod_Base;
+	class PSC: Mod_Base
+	{
+		logo    = "PSC_Core\Data\logo_ca.paa";
+		picture = "PSC_Core\Data\logo_ca.paa";
+		name    = "Project Scorched";
+	};
+};
