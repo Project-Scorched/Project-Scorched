cd7b9788f5f47d33a6eddda36ed0b103a797db8a
BeaniePope <Swifter11110@gmail.com>
2024-09-12 13:18:51 -0400

    Some basic

---
+++ @PSC/addons/PSC_Weapons/Service Rifle/config.cpp
@@ -0,0 +1,24 @@
+class CfgPatches
+{
+    class PSC_Service_Rifle
+    {
+        requiredAddons[] = 
+        {
+
+        };
+        weapons[] = 
+        {
+
+        };
+        units[] =
+        {
+
+        };
+        author = "$STR_PSC_Author";
+    };
+};
+
+class CfgWeapons
+{
+    class 
+};
--- @PSC/addons/PSC_Weapons/ammunition/config.cpp
+++ @PSC/addons/PSC_Weapons/ammunition/config.cpp
@@ -1,8 +1,8 @@
-class CfgPatches 
+class CfgPatches
 {
-	class PSC_Weapons
-	{
-		units[] = 
+    class PSC_Magazines
+    {
+        units[] = 
 		{
 
 		};
@@ -10,15 +10,21 @@ class CfgPatches
 		{
 			
 		};																								
-		requiredVersion = 0.1; // Don't Touch
-		version	= PSC_VERSION;
 		requiredAddons[] = 
-		{ 						// Loads all of Arma + CBA
-           	  "cba_main",
-          	  "A3_Data_F_AoW_Loadorder"
+		{ 						
+
     	};
 		addonRootClass = "PSC_Weapons"; // Subconfigs
 		author = "$STR_PSC_Author"; // Team name
 		fileName = "PSC_Weapons.pbo"; // File name
     };
+};
+
+class CfgAmmo
+{
+    
+};
+class CfgMagazines
+{
+
 };
