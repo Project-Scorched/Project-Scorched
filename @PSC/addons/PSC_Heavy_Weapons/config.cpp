94e6cf9bf7b9d4b316355a11078d1f3021bafbe0
gordielightfoot <gordonlightfoot4@gmail.com>
2024-09-22 12:13:46 -0400

    Added reqs

--- @PSC/addons/PSC_Heavy_Weapons/config.cpp
+++ @PSC/addons/PSC_Heavy_Weapons/config.cpp
@@ -1,1 +1,9 @@
 // Start of File
+
+/* Create a framework for heavy weapons
+add weight for movements
+add animation for movements
+
+Need: Shoulderfire Mode
+Need: Slung Heavy Weapon Mode
+Need: Heavy Melee Weapon Mode (Not a Priority)
