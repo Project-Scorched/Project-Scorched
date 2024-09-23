01e68180ac38721d3de017ed324e40b82cac7835
gordielightfoot <gordonlightfoot4@gmail.com>
2024-09-23 12:11:31 -0400

    added config.CPP Main loader

--- @PSC/addons/PSC_Heavy_Weapons/config.cpp
+++ @PSC/addons/PSC_Heavy_Weapons/config.cpp
@@ -7,3 +7,28 @@ add animation for movements
 Need: Shoulderfire Mode
 Need: Slung Heavy Weapon Mode
 Need: Heavy Melee Weapon Mode (Not a Priority)
+*/
+class CfgPatches 
+{
+	class PSC_Weapons
+	{
+		units[] = 
+		{
+
+		};
+		weapons[] = 
+		{
+			
+		};																								
+		requiredVersion = 0.1; // Don't Touch
+		version	= PSC_VERSION;
+		requiredAddons[] = 
+		{ 						// Loads all of Arma + CBA
+           	  "cba_main",
+          	  "A3_Data_F_AoW_Loadorder"
+    	};
+		addonRootClass = "PSC_Heavy_Weapons"; // Subconfigs
+		author = "$STR_PSC_Author"; // Team name
+		fileName = "PSC_Heavy_Weapons.pbo"; // File name
+    };
+};
