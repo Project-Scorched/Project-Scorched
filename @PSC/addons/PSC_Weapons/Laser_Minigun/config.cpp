9cc2583e497a55c1c2d8e23f0fef0a218440c9d3
gordielightfoot <gordonlightfoot4@gmail.com>
2024-09-23 15:07:31 -0400

    Added PSC_Heavy Weapons classes

---
+++ @PSC/addons/PSC_Weapons/Laser_Minigun/config.cpp
@@ -0,0 +1,156 @@
+// Copied and Starting Conversion from Service Rifle
+
+class CfgPatches
+{
+    class PSC_Laser_Minigun
+    {
+        requiredAddons[] = 
+        {
+			"A3_Heavy_Weapons_F" //A3 Weapons Config.
+        };
+        weapons[] = 
+        {
+			"PSC_Minigun_Base_W"
+        };
+        units[] =
+        {
+
+        };
+        author = "$STR_PSC_Author";
+		addonRootClass = "PSC_Heavy_Weapons"; // Subconfigs
+    };
+};
+
+
+class Mode_Burst;
+class Mode_FullAuto;
+class MuzzleSlot;
+class SlotInfo;
+class CowsSlot;
+class PointerSlot;
+class CowsSlot_Rail;
+class CowsSlot_Rail_Pistol;
+class CowsSlot_Pistol;
+class CowsSlot_MSBS65;
+class CowsSlot_Dovetail;
+class CowsSlot_Dovetail_RPG;
+class MuzzleSlot_45ACP;
+class MuzzleSlot_9mm;
+class MuzzleSlot_65;
+class MuzzleSlot_65MG;
+class MuzzleSlot_556;
+class MuzzleSlot_762;
+class MuzzleSlot_338;
+class MuzzleSlot_93;
+class MuzzleSlot_57;
+class MuzzleSlot_58;
+class MuzzleSlot_545R;
+class MuzzleSlot_762R;
+class UnderBarrelSlot_rail;
+class PointerSlot_Rail;
+class PointerSlot_Pistol;
+class PointerSlot_Launcher;
+class CfgHeavy_Weapons
+{
+	class Minigun;
+	class Mmigun_Base: Minigun
+	{
+		class WeaponsSlotsInfo;
+	};
+
+    class PSC_Minigun_Base:	Minigun_Base
+    {
+      	author =  "$STR_PSC_Author";
+		displayName = "Minigun_Base";
+		scope = 0;
+		magazines[] = 
+		{
+			"PSC_762_Minigun_Base"
+		};
+        magazineReloadSwitchPhase = 0.4;
+        reloadAction = "GestureReloadtemp";
+        recoil = "recoil_mk20";
+        maxZeroing = 800;
+        class WeaponSlotsInfo
+        {
+
+        };
+        distanceZoomMin = 300;
+        distanceZoomMax = 300;
+        descriptionShort = "";
+        handAnim[] = {"OFP2_ManSkeleton","\A3\Weapons_F_beta\rifles\mk20\data\Anim\mk20.rtm"};
+		bullet1[] = {"A3\sounds_f\weapons\shells\5_56\Shellcase_762_Metal_01",0.4466836,1,15};
+		bullet2[] = {"A3\sounds_f\weapons\shells\5_56\Shellcase_762_Metal_02",0.4466836,1,15};
+		bullet3[] = {"A3\sounds_f\weapons\shells\5_56\Shellcase_762_Metal_03",0.4466836,1,15};
+		bullet4[] = {"A3\sounds_f\weapons\shells\5_56\Shellcase_762_Metal_04",0.4466836,1,15};
+		bullet5[] = {"A3\sounds_f\weapons\shells\5_56\Shellcase_762_Dirt_01",0.4466836,1,15};
+		bullet6[] = {"A3\sounds_f\weapons\shells\5_56\Shellcase_762_Dirt_02",0.4466836,1,15};
+		bullet7[] = {"A3\sounds_f\weapons\shells\5_56\Shellcase_762_Dirt_03",0.4466836,1,15};
+		bullet8[] = {"A3\sounds_f\weapons\shells\5_56\Shellcase_762_Dirt_04",0.4466836,1,15};
+		bullet9[] = {"A3\sounds_f\weapons\shells\5_56\Shellcase_762_Grass_01",0.4466836,1,15};
+		bullet10[] = {"A3\sounds_f\weapons\shells\5_56\Shellcase_762_Grass_02",0.4466836,1,15};
+		bullet11[] = {"A3\sounds_f\weapons\shells\5_56\Shellcase_762_Grass_03",0.4466836,1,15};
+		bullet12[] = {"A3\sounds_f\weapons\shells\5_56\Shellcase_762_Grass_04",0.4466836,1,15};
+		soundBullet[] = {"bullet1",0.087,"bullet2",0.083,"bullet3",0.083,"bullet4",0.083,"bullet5",0.093,"bullet6",0.093,"bullet7",0.073,"bullet8",0.073,"bullet9",0.083,"bullet10",0.083,"bullet11",0.083,"bullet12",0.083};
+		modes[] = {"Single","FullAuto","fullauto_medium","single_medium_optics1","single_far_optics2"};
+		drySound[] = {"A3\Sounds_F\arsenal\weapons\Rifles\Mk20\dry_Mk20",0.31622776,1,10};
+		reloadMagazineSound[] = {"A3\Sounds_F\arsenal\weapons\",1.0,1,10};
+		changeFiremodeSound[] = {"A3\Sounds_F\arsenal\weapons\",0.25118864,1,5};
+		class FullAuto: Mode_FullAuto
+		{
+			sounds[] = {"StandardSound"};
+			class BaseSoundModeType{};
+			class StandardSound: BaseSoundModeType{};
+			class SilencedSound: BaseSoundModeType{};
+			reloadTime = 0.20;
+			dispersion = 0.00116;
+			recoil = "recoil_auto_mk20";
+			recoilProne = "recoil_auto_prone_mk20";//Remove Prone from minigun style heavy weapons?
+			minRange = 2;
+			minRangeProbab = 0.9;
+			midRange = 15;
+			midRangeProbab = 0.7;
+			maxRange = 30;
+			maxRangeProbab = 0.1;
+			aiRateOfFire = 1e-06;
+		};
+    };
+
+	class PSC_LaserMinigun_Base_W: PSC_Minigun_Base_W
+	{
+		displayName = "Laser Minigun";
+		model = "PSC_Weapons\Laser_Minigun\LaserMinigun.p3d";
+		scope = 2;
+		descriptionShort = "Short Description Here";//Need to Add Short Description
+		hiddenSelections[] = 
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\PSC_Weapons\Laser_Minigun\data\LaserMinigun_co.paa"
+		};
+		hiddenSelectionsMaterials[] = 
+		{
+			"\PSC_Weapons\Laser_Minigun\data\LaserMinigun.rvmat"
+		};
+	};
+	class PSC_LaserMinigun_Accurate: PSC_Minigun_Base_W
+	{	
+		displayName = "Accurate Laser Minigun";
+		scope = 2;
+		descriptionShort = "Short Description Here";// Need to Add Short Description
+		hiddenSelections[] = 
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\PSC_Weapons\Laser_Minigun\data\polymer\LaserMinigun_co.paa"
+		};
+		hiddenSelectionsMaterials[] = 
+		{
+			"\PSC_Weapons\Laser_Minigun\data\polymer\LaserMinigun.rvmat"
+		};
+	}
+};
