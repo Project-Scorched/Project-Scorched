799d9e332013dce8461455359e6cb35c3cf67fd2
BeaniePope <Swifter11110@gmail.com>
2024-09-25 13:45:40 -0400

    changes

--- @PSC/addons/PSC_Weapons/Service_Rifle/config.cpp
+++ @PSC/addons/PSC_Weapons/Service_Rifle/config.cpp
@@ -77,7 +77,7 @@ class CfgWeapons
         distanceZoomMin = 300;
         distanceZoomMax = 300;
         descriptionShort = "";
-        handAnim[] = {"OFP2_ManSkeleton","\A3\Weapons_F_beta\rifles\mk20\data\Anim\mk20.rtm"};
+        handAnim[] = {"OFP2_ManSkeleton","PSC_Weapons\Service_Rifle\data\animations\serviceriflehold.rtm"};
 		bullet1[] = {"A3\sounds_f\weapons\shells\5_56\Shellcase_556_Metal_01",0.4466836,1,15};
 		bullet2[] = {"A3\sounds_f\weapons\shells\5_56\Shellcase_556_Metal_02",0.4466836,1,15};
 		bullet3[] = {"A3\sounds_f\weapons\shells\5_56\Shellcase_556_Metal_03",0.4466836,1,15};
@@ -166,9 +166,9 @@ class CfgWeapons
 		};
 		hiddenSelectionsMaterials[] = 
 		{
-			"\PSC_Weapons\Service_Rifle\data\polymer\ServiceRifle.rvmat"
+			"\PSC_Weapons\Service_Rifle\data\polymer\ServiceRiflePolymer.rvmat"
 		};
-	}
+	};
 };
 
 
