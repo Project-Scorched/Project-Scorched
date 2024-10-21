fc0e183191f07d1ee25eac09200451ddc3cf217c
BeaniePope <Swifter11110@gmail.com>
2024-10-21 15:24:19 -0400

    logo!

---
+++ @PSC/mod.cpp
@@ -0,0 +1,14 @@
+name			= "Project Scorched";					// Name of your mod
+author			= "Project Scorched Team";				// Affects Arma 3 Launcher, when the mod are loaded as local
+logo			= "PSC_Core\Data\logo_ca.paa";			// Logo displayed in the main menu
+logoOver		= "PSC_Core\Data\logo_ca.paa";	// When the mouse is over, in the main menu
+tooltip			= "Project Scorched";
+tooltipOwned	= "I'm very tired";				// Tool tip displayed when the mouse is left over, in the main menu
+picture			= "";		// Picture displayed from the expansions menu. Optimal size is 2048x1024
+actionName		= "GitHub";
+action			= "https://github.com/BeaniePope/Project-Scorched";			// Website URL, that can be accessed from the expansions menu
+overview		= "A Fallout Inspired mod for Arma 3";	// Supports structured text
+hideName		= 0;						// Hide the extension name
+hidePicture		= 0;						// Hide the extension menu
+dlcColor[]		= {255, 210, 0, 0.8};	// Color used for DLC stripes and backgrounds (RGBA)
+logoSmall		= "PSC_Core\Data\logo_ca.paa";		// Display in creative lists, next to the entities added by the mod
