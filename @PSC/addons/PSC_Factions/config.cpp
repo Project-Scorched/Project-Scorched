eb0f2de43322db12b430d1ae24763a53077f9e91
BeaniePope <Swifter11110@gmail.com>
2024-11-18 15:45:15 -0500

    Factions CPP

---
+++ @PSC/addons/PSC_Factions/config.cpp
@@ -0,0 +1,23 @@
+class CfgPatches 
+{
+  class PSC_Factions
+  {
+      requiredAddons[] = 
+      {
+          "PSC_Core"
+      };
+      units[] = 
+      {
+  
+      };
+      weapons[] = 
+      {
+  
+      };
+      requiredVersion = 0.1;
+      version = 0.1;
+      addonRootClass = "PSC_Factions";
+      author = "$STR_PSC_Author";
+      fileName = "PSC_Factions"; 
+  };
+};
